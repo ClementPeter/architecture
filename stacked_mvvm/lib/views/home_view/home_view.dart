9dfc895f1827688a6e8ffe1ffe6260c0c9e8fd6d
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2023-03-20 23:46:59 +0100

    💡 setup: Stacked Architecture_MVVM

--- stacked_mvvm/lib/views/home_view/home_view.dart
+++ stacked_mvvm/lib/views/home_view/home_view.dart
@@ -1,29 +1,29 @@
-// import 'package:flutter/src/widgets/framework.dart';
-// import 'package:flutter/src/widgets/container.dart';
-// import 'package:flutter/material.dart';
-// import 'package:stacked/stacked.dart';
-// import 'package:stacked_mvvm/views/home_view/home_viewModel.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked_mvvm/views/home_view/home_viewModel.dart';
 
 
-// //View hold the UI of the home
+//View hold the UI of the home
 
-// class HomeView extends StatelessWidget {
-//   const HomeView({super.key});
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return ViewModelBuilder.reactive(
-//       viewModelBuilder: () => HomeViewModel(),
-//       builder: (context, model, child) => Scaffold(
-//         body: Center(
-//           child: Text(model.title),
-//         ),
-//         floatingActionButton: FloatingActionButton(
-//           onPressed: () {
-//             model.updateCounter();
-//           },
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder.reactive(
+      viewModelBuilder: () => HomeViewModel(),
+      builder: (context, model, child) => Scaffold(
+        body: Center(
+          child: Text(model.title),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            model.updateCounter();
+          },
+        ),
+      ),
+    );
+  }
+}
